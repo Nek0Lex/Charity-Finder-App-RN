afb460e9d121a7bb46469200c0d77f5801939d9f
Alex Wong <wonghoming111105@gmail.com>
2023-09-27 02:43:23 -0400

    update detail

--- screen/FavouriteScreen.js
+++ screen/FavouriteScreen.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -7,37 +7,65 @@ import {
   Platform,
   StatusBar,
   TextInput,
-  Button,
   FlatList,
+  Pressable,
 } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Card, Button } from 'react-native-paper';
+
 
 export default FavouriteScreen = ({ navigation }) => {
   const [favouriteList, setFavouriteList] = useState([]);
 
   useEffect(() => {
-    getAllData();
-  }, []);
-
-  const getAllData = async () => {
-    let keys = [];
-    try {
-      keys = await AsyncStorage.getAllKeys();
-      keys.map(async (k) => {
-        const detail = await AsyncStorage.getItem(k);
-        setFavouriteList([...favouriteList, detail]);
-      });
-    } catch (e) {
-      // read key error
+    const fetchData = async () => {
+      const keys = await AsyncStorage.getAllKeys();
+      let dataArray = [];
+      for (let key of keys) {
+        let data = await AsyncStorage.getItem(key);
+        dataArray.push(JSON.parse(data))
+      }
+      setFavouriteList(dataArray)
     }
 
-    console.log(keys);
-  };
+    fetchData()
+  }, []);
+
+  const clearItem = async (ein) => {
+    try {
+      await AsyncStorage.removeItem(ein)
+    } catch (e) {
+      console.log(e)
+    }
+    console.log(`Removed ${ein}.`)
+  }
+
+  const CardItem = ({ detail }) => (
+    <Card style={{
+      display: 'flex',
+      flexDirection: 'column',
+      marginHorizontal: 15
+    }}>
+      <Text>{detail.name}</Text>
+      <Button onPress={() => {
+        setFavouriteList(
+          favouriteList.filter((fav) => fav.ein != detail.ein)
+        );
+        clearItem(detail.ein)
+      }}>Remove</Button>
+    </Card>
+  );
 
   return (
     <View>
       <FlatList
         data={favouriteList}
-        renderItem={({ item }) => <Text>${item}</Text>}
+        renderItem={({ item }) => (
+          <CardItem detail={item} />
+        )}
+        ItemSeparatorComponent={() => <View style={{ height: 15 }} />}
+        ListFooterComponent={() => <View style={{ height: 15 }} />}
+        ListHeaderComponent={() => <View style={{ height: 15 }} />}
       />
     </View>
   );
